3d79f051a8e82ab1af10dd599101dbd9340af9b9
Vivek Singh <vivek.99.singh@gmail.com>
2023-09-03 12:39:19 +0300

    I am carrying out changes and  added extra line

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 
-## The below block of code creates new resource group
+## The below block of code creates new resource group in Azure subscription
 resource "azurerm_resource_group" "rg" {
   name     = "testrg"
   location = "West Europe"
 }
-
-
-
-
-
