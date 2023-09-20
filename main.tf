ab747fa4af50a9d0f4fa97ba65dc60240b9a544e
Vivek Singh <vivek.99.singh@gmail.com>
2023-09-20 23:10:02 +0300

    fix for error encountered

--- main.tf
+++ main.tf
@@ -14,23 +14,7 @@ resource "azurerm_storage_account" "stg" {
   account_replication_type = "LRS"
 }
 
-# Local provisioner to configure the Terraform backend after resources are created
-provisioner "local-exec" {
-  command = <<EOT
-  cat <<EOF > backend.tf
-  terraform {
-    backend "azurerm" {
-      resource_group_name   = "${azurerm_resource_group.rg.name}"
-      storage_account_name  = "${azurerm_storage_account.stg.name}"
-      container_name        = "tfstate"
-      key                   = "terraform.tfstate"
-    }
-  }
-  EOF
-  EOT
-}
-
-# Trigger the provisioner only once after the Resource Group and Storage Account are created
+# Configure the Terraform backend after the resources are created
 resource "null_resource" "configure_backend" {
   triggers = {
     resource_group_id = azurerm_resource_group.rg.id
@@ -38,7 +22,18 @@ resource "null_resource" "configure_backend" {
   }
 
   provisioner "local-exec" {
-    command = "echo 'Terraform backend configured.'"
+    command = <<EOT
+    cat <<EOF > backend.tf
+    terraform {
+      backend "azurerm" {
+        resource_group_name   = "${azurerm_resource_group.rg.name}"
+        storage_account_name  = "${azurerm_storage_account.stg.name}"
+        container_name        = "tfstate"
+        key                   = "devterraform.tfstate"
+      }
+    }
+    EOF
+    EOT
   }
 }
 
