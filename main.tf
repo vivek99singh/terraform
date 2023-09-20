e60a85a16dd53f8c423ebc13fd48eb4729c4be96
Vivek Singh <vivek.99.singh@gmail.com>
2023-09-20 23:07:09 +0300

    Made changes again

--- main.tf
+++ main.tf
@@ -14,14 +14,31 @@ resource "azurerm_storage_account" "stg" {
   account_replication_type = "LRS"
 }
 
-# Configure the Terraform backend to use Azure Blob Storage
-terraform {
-  backend "azurerm" {
-    resource_group_name   = azurerm_resource_group.rg.name
-    storage_account_name  = azurerm_storage_account.stg.name
-    container_name        = "tfstate"
-    key                   = "devterraform.tfstate"
+# Local provisioner to configure the Terraform backend after resources are created
+provisioner "local-exec" {
+  command = <<EOT
+  cat <<EOF > backend.tf
+  terraform {
+    backend "azurerm" {
+      resource_group_name   = "${azurerm_resource_group.rg.name}"
+      storage_account_name  = "${azurerm_storage_account.stg.name}"
+      container_name        = "tfstate"
+      key                   = "terraform.tfstate"
+    }
   }
+  EOF
+  EOT
 }
 
+# Trigger the provisioner only once after the Resource Group and Storage Account are created
+resource "null_resource" "configure_backend" {
+  triggers = {
+    resource_group_id = azurerm_resource_group.rg.id
+    storage_account_id = azurerm_storage_account.stg.id
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Terraform backend configured.'"
+  }
+}
 
