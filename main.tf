3f07067030fb2859f5eefbba3682f3cf02fca7f9
Vivek Singh <vivek.99.singh@gmail.com>
2023-09-20 22:17:29 +0300

    Code changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 ## The below block of code creates new resource group in Azure subscription
 resource "azurerm_resource_group" "rg" {
-  name     = "testrg"
+  name     = "testrg1"
   location = "West Europe"
 }
 
