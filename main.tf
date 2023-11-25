8fbddb83878b72d617f5ded36f4c29d111397e77
Vivek Singh <vivek.99.singh@gmail.com>
2023-11-25 02:18:47 +0200

    made chnages to test

--- main.tf
+++ main.tf
@@ -14,27 +14,5 @@ resource "azurerm_storage_account" "stg" {
   account_replication_type = "LRS"
 }
 
-# Configure the Terraform backend after the resources are created
-resource "null_resource" "configure_backend" {
-  triggers = {
-    resource_group_id = azurerm_resource_group.rg.id
-    storage_account_id = azurerm_storage_account.stg.id
-  }
 
-
-  provisioner "local-exec" {
-    command = <<EOT
-    cat <<EOF > backend.tf
-    terraform {
-      backend "azurerm" {
-        resource_group_name   = "${azurerm_resource_group.rg.name}"
-        storage_account_name  = "${azurerm_storage_account.stg.name}"
-        container_name        = "tfstate"
-        key                   = "devterraform.tfstate"
-      }
-    }
-    EOF
-    EOT
-  }
-}
 
