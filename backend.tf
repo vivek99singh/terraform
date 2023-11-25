7ca001f0190f86f2f3d779ee220d3dd4c3ce9482
Vivek Singh <vivek.99.singh@gmail.com>
2023-11-25 02:45:28 +0200

    md ch

--- backend.tf
+++
@@ -1,9 +0,0 @@
-    terraform {
-      backend "azurerm" {
-        resource_group_name   = "myrg"
-        storage_account_name  = "dvstg991983"
-        container_name        = "tfstate"
-        key                   = "devterraform.tfstate"
-      }
-    }
-    EOF
